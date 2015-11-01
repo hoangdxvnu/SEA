5ab49f10acba10790b96a21130708f895cfa1dbc
hoangdxvnu <hoangdx12@gmail.com>
2015-11-02 01:02:25 +0700

    Add Token to SecureString

--- TLU.Blog/Models/DataBase/ThangLongEntities.Designer.cs
+++ TLU.Blog/Models/DataBase/ThangLongEntities.Designer.cs
@@ -1,4 +1,4 @@
-﻿// T4 code generation is enabled for model 'E:\TLU.Blog\TLU.Blog\TLU.Blog\Models\DataBase\ThangLongEntities.edmx'. 
+﻿// T4 code generation is enabled for model 'E:\WINDOWS DATA\.NET Advanced\TLU\Source Code Blog\TLU.Blog\Models\DataBase\ThangLongEntities.edmx'. 
 // To enable legacy code generation, change the value of the 'Code Generation Strategy' designer
 // property to 'Legacy ObjectContext'. This property is available in the Properties Window when the model
 // is open in the designer.
